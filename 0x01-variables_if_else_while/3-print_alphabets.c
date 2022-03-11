458587fbe0edf19f81bad2c346e27d1b125244b2
mzdee2906 <edibabe2906@gmail.com>
2022-03-11 06:49:10 -0800

    Print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char a = 'a';
+	char b = 'A';
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (b = 'A'; b <= 'z'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n')
+		return (0);
+}
