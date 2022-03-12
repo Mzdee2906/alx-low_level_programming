f1c3450e4137142260652861a8cff58ecbf186a7
mzdee2906 <edibabe2906@gmail.com>
2022-03-11 17:52:10 -0800

    Print base sixteen

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char a = '0';
+	char 1 = 'a';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (1 = 'a'; 1 <= 'f'; 1++)
+	{
+		putchar(1);
+	}
+	putchar('\n');
+	return (0);
+}
